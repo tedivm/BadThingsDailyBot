a303818e72ee76d4a0c6bc4872794cc1c156ff6c
Robert Hafner <tedivm@tedivm.com>
2018-06-17 20:19:41 -0700

    Make a slack button

--- badthings/routes/auth.py
+++ badthings/routes/auth.py
@@ -1,13 +1,14 @@
 from badthings import app
 from badthings.services import slack
 
+AUTH_BUTTON='<a href="%s"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
 
 @app.route("/slack/install", methods=["GET"])
 def pre_install():
-    return '<a href="%s">Install App</a>' % (slack.get_auth_link())
+    return AUTH_BUTTON % (slack.get_auth_link())
 
 
-@app.route("/slack/sucessful_install")
+@app.route("/slack/successful_install")
 def sucessful_install():
     return 'Install Successfull!'
 
