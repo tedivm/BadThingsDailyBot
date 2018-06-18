7416c4a7e2b36a95b69e9231bc57f6d8839f2edd
Robert Hafner <tedivm@tedivm.com>
2018-06-17 22:38:05 -0700

    Add "commands" to app scope

--- badthings/services/slack.py
+++ badthings/services/slack.py
@@ -3,7 +3,7 @@ from flask import request, url_for
 from slackclient import SlackClient
 
 sc = SlackClient(app.config['slack']['token'])
-SCOPE='chat:write:bot'
+SCOPE='chat:write:bot,commands'
 AUTH_BUTTON='<a href="%s"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>'
 
 def send_message(message, channel):
